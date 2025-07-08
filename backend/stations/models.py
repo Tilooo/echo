53f890c4a7017abe06e16eac1dd30273487edd78
Tilooo <moonaviad@gmail.com>
2025-07-08 17:28:47 +0300

    feat: Define Station model and create initial migration

--- backend/stations/models.py
+++ backend/stations/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Station(models.Model):
+    name = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    stream_url = models.CharField(max_length=500, unique=True)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
+    def __str__(self):
+        return f"{self.name} ({self.country})"
