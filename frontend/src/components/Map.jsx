88b4a298c2c436c5fe2c7832558d237b5850ba33
Tilooo <moonaviad@gmail.com>
2025-07-10 22:26:36 +0300

    14. Stayled map markers.

--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -4,17 +4,19 @@ import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
-import icon from 'leaflet/dist/images/marker-icon.png';
-import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+import './CustomMarker.css'; // The CSS file
 
-let DefaultIcon = L.icon({
-    iconUrl: icon,
-    shadowUrl: iconShadow,
-    iconAnchor: [12, 41]
-});
-L.Marker.prototype.options.icon = DefaultIcon;
+// a custom div icon
+const createCustomIcon = () => {
+  return L.divIcon({
+    html: `<div class="custom-marker-icon"></div>`,
+    className: 'custom-marker-container',
+    iconSize: [12, 12],
+    iconAnchor: [6, 6],
+  });
+};
 
-// The component now accepts 'onStationSelect' as a prop
+// Map Component
 const Map = ({ stations, onStationSelect }) => {
   const position = [51.505, -0.09];
 
@@ -24,19 +26,20 @@ const Map = ({ stations, onStationSelect }) => {
         url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
         attribution='© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors © <a href="https://carto.com/attributions">CARTO</a>'
       />
-
       {stations.map(station => (
         <Marker
           key={station.id}
           position={[station.latitude, station.longitude]}
+          icon={createCustomIcon()}
         >
           <Popup>
-            <b>{station.name}</b><br />
-            {station.country}<br />
-            {/* a button that calls the onStationSelect function */}
-            <button onClick={() => onStationSelect(station)}>
-              Play
-            </button>
+            <div>
+              <b>{station.name}</b><br />
+              {station.country}<br />
+              <button onClick={() => onStationSelect(station)}>
+                Play
+              </button>
+            </div>
           </Popup>
         </Marker>
       ))}
@@ -45,3 +48,4 @@ const Map = ({ stations, onStationSelect }) => {
 };
 
 export default Map;
+
